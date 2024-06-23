9266eec5809bf96f6d1f8cd64e32725f96aa65fb
vikashjo <vikashjoshi431@gmail.com>
2024-06-23 21:53:41 +0530

    Add views and style

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,15 @@
 class Task < ApplicationRecord
   belongs_to :user
   enum status: { Backlog: 0, In_progress: 1, Done: 2 }
+
+  def badge_color
+    case status
+    when 'In_progress'
+      'secondary'
+    when 'Backlog'
+      'info'
+    when 'Done'
+      'success'
+    end
+  end
 end
